e70e7ee79617ee5b41f907e6060f73dd4ea5fd95
Daniel <mr_schliesing@hotmail.com>
2022-02-10 20:52:54 -0900

    css is not my favorite
    copy pasta'd some css and made a new comment specific css file. thanks this person https://bbbootstrap.com/snippets/bootstrap-comments-list-font-awesome-icons-and-toggle-button-91650380

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,15 +1,26 @@
 import React from 'react'
+import '../comment.css'
 
 const Comment = (props) => {
-
+	//https://bbbootstrap.com/snippets/bootstrap-comments-list-font-awesome-icons-and-toggle-button-91650380
 	const { commentText, commentPoster, repliedTo, timePosted } = props.userComment
 
 	return (
-		<div className='Comment  cards'>
-		<img src="#" alt="profile picture" className="icons" />
-		<h4>{commentPoster}</h4>
-		<p>{timePosted}</p>
-		<p>{commentText}</p>
+		<div className='Comment  card p-3'>
+			<div className="d-flex justify-content-between align-items-center">
+                    <div className="user d-flex flex-row align-items-center">
+	                    <img src="#" alt="profile picture " width="30" className="user-img rounded-circle mr-2" /> 
+	                    <span>
+	                    	<small className="font-weight-bold text-primary">{commentPoster}</small>
+	                    	<small className="font-weight-bold"><p>{commentText}</p></small>
+	                    </span>
+                    </div>
+                    <small>{timePosted}</small>
+                </div>
+                <div className="action d-flex justify-content-between mt-2 align-items-center">
+                    <div className="reply px-4"> <small>Reply</small> </div>
+                    <div className="icons align-items-center"> <i className="fa fa-star text-warning"></i> <i className="fa fa-check-circle-o check-icon"></i> </div>
+                </div>
 		</div>
 
 
